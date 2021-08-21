fec7b908e13e21db884b647cee670bdc656c09b5
JPujitha <88589553+JPujitha@users.noreply.github.com>
2021-08-21 22:21:38 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,14 @@
+package week3.Day1.Assignment.org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("***********college package**********");
+		System.out.println("collegeName method from College class");
+	}
+public void collegeCode() {
+	System.out.println("collegeCode method in College class");
+}
+public void collegeRank() {
+	System.out.println("collegeRank method in College class");
+}
+}
